217f3a2fba5f57169aafc3e20a8023f0bd410a27
JPlukarski <jwplukarski@gmail.com>
2018-08-15 19:09:47 -0500

    finished java and css

--- assets/app.js
+++ assets/app.js
@@ -1,39 +1,89 @@
-//Initial array of animals
-var animals = [
-    "Lions",
-    "Tigers",
-    "Bears",
-    "Dogs",
+//Initial array of desserts
+var desserts = [
+    "Cookies",
+    "Pies",
+    "Doughnuts",
+    "Cakes",
 ];
 
 //API Stuff
+function displayDessertsGifs() {
+    $("#desserts").empty();
+    var dessert = $(this).attr("data-name");
+    var limit = 10;
+    console.log("Hello!")
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dessert + "&limit=" + limit + "&api_key=gLeB5PHXzjgIpt57b5y35Y0ukNAkv14k";
+    console.log(queryURL);
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
 
-var animalGif
+        //get the rating of each dessert gif
+        for (i = 0; i < limit; i++) {
+            //get the rating of the gif
+            var rating = response.data[i].rating;
+            console.log(rating);
+            var dessertDiv = $("<div class='dessert'>");
+            var pRatingOne = $("<p>").text("Rating: " + rating);
+            dessertDiv.append(pRatingOne);
+            $("#desserts").prepend(dessertDiv);
 
-//Function for displaying animal data 
+            //get the still and animated images of the gif
+
+            var originalStill = response.data[i].images.original_still.url;
+            var animatedGif = response.data[i].images.original.url;
+            console.log("originalStill is: " + originalStill);
+            var pStillImage = $("<img>").attr({ src: originalStill, id: 'images' + i, "data-state": "still", "data-still": originalStill, "data-animate": animatedGif, class: "images" });
+            dessertDiv.append(pStillImage);
+            console.log("animatedGif is: " + animatedGif);
+            //Clicking on the still gif animates it and vice versa
+            $("#images" + i).on("click", function () {
+                var state = $(this).attr("data-state");
+                console.log("This is the State: " + state);
+                console.log(this);
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                };
+            });
+
+        };
+    });
+
+};
+
+//Function for displaying dessert button
 function renderButtons() {
 
-    $("#animalButtons").empty();
+    $("#dessertButtons").empty();
 
-    //Loops through the animal array and creates buttons for each
-    for (var i = 0; i < animals.length; i++) {
+    //Loops through the desserts array and creates buttons for each
+    for (var i = 0; i < desserts.length; i++) {
         var newButton = $("<button>");
-        newButton.addClass("animal");
-        newButton.attr("data-name", animals[i]);
-        newButton.text(animals[i]);
-        $("#animalButtons").append(newButton);
+        newButton.addClass("dessertButton");
+        newButton.attr("data-name", desserts[i]);
+        newButton.text(desserts[i]);
+        $("#dessertButtons").append(newButton);
     }
 };
 
-//Function for making the "submit" button add an animal to the array
+//Function for making the "submit" button add an dessert to the array
 
-$("#addAnimal").on("click", function (event) {
+$("#addDessert").on("click", function (event) {
     event.preventDefault();
-    var animal = $("#animalInput").val().trim();
-    animals.push(animal);
+    var dessert = $("#dessertInput").val().trim();
+    desserts.push(dessert);
     renderButtons();
 });
 
-//Calls the initial list of animals
+//Click event listener for all added dessert buttons
+$(document).on("click", ".dessertButton", displayDessertsGifs);
+
+//Calls the initial list of desserts
 renderButtons();
+
